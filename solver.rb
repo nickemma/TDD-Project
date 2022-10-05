86f870c38abf793b5146df4ff2548fa4fabfc9ed
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2022-10-05 16:20:38 +0100

    done

--- solver.rb
+++ solver.rb
@@ -1,29 +1,29 @@
-class Solver
-  def factorial(num)
-    if num.negative?
-      'Please enter positive number or equal to 0!'
+# class Solver
+#   def factorial(num)
+#     if num.negative?
+#       'Please enter positive number or equal to 0!'
 
-    elsif [0, 1].include?(num)
-      1
-    else
+#     elsif [0, 1].include?(num)
+#       1
+#     else
 
-      num * factorial(num - 1)
-    end
-  end
+#       num * factorial(num - 1)
+#     end
+#   end
 
-  def reverse(str)
-    str.reverse
-  end
+#   def reverse(str)
+#     str.reverse
+#   end
 
-  def fizz_buzz(num)
-    if (num % 5).zero? && (num % 3).zero?
-      'fizz_buzz'
-    elsif (num % 3).zero?
-      'fizz'
-    elsif (num % 5).zero?
-      'buzz'
-    else
-      num.to_s
-    end
-  end
-end
+#   def fizz_buzz(num)
+#     if (num % 5).zero? && (num % 3).zero?
+#       'fizz_buzz'
+#     elsif (num % 3).zero?
+#       'fizz'
+#     elsif (num % 5).zero?
+#       'buzz'
+#     else
+#       num.to_s
+#     end
+#   end
+# end
